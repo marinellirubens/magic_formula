d1817baabaefd5e57e8a32c03e2442a1cf6d54af
marinellirubens <marinelli.rubens@gmail.com>
2021-07-09 23:31:18 -0300

    Removed setup tests

--- tests/test_stocks_greenblat_magic_formula.py
+++ tests/test_stocks_greenblat_magic_formula.py
@@ -24,24 +24,6 @@ def scenario_logger():
 
 
 class TestStocksGreenblatMagicFormula(unittest.TestCase):
-    def test_get_config(self):
-        config = green.get_config('tests/teste.json')
-        self.assertIsNotNone(config)
-        self.assertIsInstance(config, dict)
-        self.assertEqual(config['BRX10_URL'], 'https://statusinvest.com.br/indices/indice-brasil-100')
-        
-    def test_set_logger(self):
-        logger = scenario_logger()
-        logger_formated: logging.Logger = green.set_logger(logger)
-        
-        self.assertEqual(logger_formated.level, 10)
-        self.assertIsInstance(logger_formated.handlers[0], logging.handlers.RotatingFileHandler)
-        self.assertIsInstance(logger_formated.handlers[1], logging.StreamHandler)
-
-        format = '%(asctime)s - %(name)s - (%(threadName)-10s) - %(levelname)s - %(message)s'
-        for handler in logger_formated.handlers:
-            self.assertEqual(handler.formatter._fmt, format)
-
     def test_get_ticker_info_assert_is_dict(self):
         logger = scenario_logger()
         logger: logging.Logger = green.set_logger(logger)
