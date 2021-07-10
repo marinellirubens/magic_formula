030279e1a84d9683d62e696a073137a06318ea78
marinellirubens <marinelli.rubens@gmail.com>
2021-07-09 23:12:15 -0300

    - Included file src/setup.py with setup functions removed from file src/stocks_greenblat_magic_formula.py - Changes on file src/stocks_greenblat_magic_formula.py: 	- Removed setup functions to be included on file src/setup.py 	- Included imports from setup.py file

--- src/stocks_greenblat_magic_formula.py
+++ src/stocks_greenblat_magic_formula.py
@@ -30,6 +30,8 @@ import requests
 import yahooquery
 from pandas import DataFrame
 
+from setup import get_config
+from setup import set_logger
 
 MAX_NUMBER_THREADS = 10
 XLSX_PATH = os.path.join(os.getcwd(), 'xlsx_files/')
@@ -61,46 +63,6 @@ def main(logger: logging.Logger = logging.getLogger(__name__)):
         excel_writer=excel_file_name,
         sheet_name='stocks', index=False, engine='openpyxl', freeze_panes=(1, 0),
     )
-
-
-def get_config(config_file: str = os.path.join(os.path.dirname(__file__), 'config.json')) -> dict:
-    """Returns the configuration from a config file
-
-    :param config_file: json file with the configurations
-    :type config_file: str
-    :return: dictionary with the configurations
-    :rtype: dict
-    """
-    with open(config_file) as file:
-        config = json.load(file)
-
-    return config
-
-
-def set_logger(logger: logging.Logger, log_file_name: str = 'stocks.log') -> logging.Logger:
-    """Sets the logger configuration
-
-    :param logger: Logger variable
-    :type logger: logging.Logger
-    :param log_file_name: name of the log file, defaults to 'stocks.log'
-    :type log_file_name: str, optional
-    :return: logger object
-    :rtype: logging.Logger
-    """
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - (%(threadName)-10s) - %(levelname)s - %(message)s')
-    handler = logging.handlers.RotatingFileHandler(log_file_name, maxBytes=1024 * 1000,
-                                                   backupCount=10)
-    buff_handler = logging.StreamHandler(sys.stdout)
-
-    handler.setFormatter(formatter)
-    buff_handler.setFormatter(formatter)
-    
-    logger.setLevel('DEBUG')
-    logger.addHandler(handler)
-    logger.addHandler(buff_handler)
-    
-    return logger
 
 
 def get_ticker_info(symbol: str, logger: logging.Logger) -> yahooquery.Ticker:
