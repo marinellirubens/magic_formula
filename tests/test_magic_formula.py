059bdd052bf01974c1cc5ae440fd0646a00ce9b9
marinellirubens <marinelli.rubens@gmail.com>
2021-07-13 11:18:21 -0300

    Improved tests for method magic_formula

--- tests/test_magic_formula.py
+++ tests/test_magic_formula.py
@@ -4,12 +4,15 @@ import sys
 import unittest
 import logging
 import yahooquery
+import pytest
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../tests')))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../tests')))
 
 
-from src.magic_formula import MagicFormula
+from src.magic_formula import MagicFormula, TICKER_INFO
 from src.setup import set_logger
 
 
@@ -17,30 +20,154 @@ class TestMagicFormula(unittest.TestCase):
     def setUp(self):
         self.logger = logging.Logger(__name__)
         self.logger = set_logger(self.logger)
-        
-    def test_magic_formula_instance(self):
-        itsa = MagicFormula('ITSA4.SA', self.logger)
 
-        self.assertIsInstance(itsa, MagicFormula)
-        self.assertEqual(itsa.symbol, 'ITSA4.SA')
-        self.assertIs(itsa.logger, self.logger)
-        self.assertIsNone(itsa.ticker_info)
+    def test_magic_formula_instance(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+
+        self.assertIsInstance(wege, MagicFormula)
+        self.assertEqual(wege.symbol, 'WEGE3.SA')
+        self.assertIs(wege.logger, self.logger)
+        self.assertIsNone(wege.ticker_info)
 
     def test_validate_ticker_info(self):
-        itsa = MagicFormula('ITSA4.SA', self.logger)
-        itsa.get_ticker_info()
-        self.assertIsNotNone(itsa.ticker)
-        self.assertIsInstance(itsa.ticker, yahooquery.Ticker)
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+        self.assertIsNotNone(wege.ticker)
+        self.assertIsInstance(wege.ticker, yahooquery.Ticker)
 
     def test_valid_recomendation_trend(self):
-        itsa = MagicFormula('ITSA4.SA', self.logger)
-        itsa.get_ticker_info()
-        itsa.get_recomendation_trend()
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+        wege.get_recomendation_trend()
         # TODO: Imcluir testes de quando esta valido
-        self.assertIsInstance(itsa.recommendation_trend, tuple)
+        self.assertIsInstance(wege.recommendation_trend, tuple)
+
+    def test_valid_recomendation_trend_error(self):
+        wege = MagicFormula('NEOE3.SA', self.logger)
+        wege.get_ticker_info()
+
+        wege.get_recomendation_trend()
+
+        self.assertIsInstance(wege.recommendation_trend, tuple)
+        self.assertEqual(wege.recommendation_trend, (0, 0))
+
+    def test_fill_key_statistics(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+
+        wege.fill_key_statistics()
+
+        self.assertIsNotNone(wege.key_statistics)
+        self.assertIsInstance(wege.key_statistics, dict)
+        self.assertNotEqual(wege.key_statistics, {})
 
     def test_valid_ticker_info(self):
-        itsa = MagicFormula('ITSA4.SA', self.logger)
-        itsa.get_ticker_info()
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
 
-        self.assertFalse(itsa.valid_ticker_info())
+        self.assertFalse(wege.valid_ticker_info())
+
+    def test_get_ebit(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+        wege.fill_ebit()
+
+        self.assertIsNotNone(wege.ebit)
+        self.assertIsInstance(wege.ebit, int)
+        self.assertGreater(wege.ebit, 0)
+
+    def test_fill_balance_sheet(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+        wege.fill_balance_sheet()
+
+        self.assertIsNotNone(wege.balance_sheet)
+        self.assertIsInstance(wege.balance_sheet, dict)
+        self.assertIsNot(wege.balance_sheet, {})
+
+    def test_valid_market_cap(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+        return_ = wege.valid_market_cap()
+
+        self.assertIsNotNone(return_)
+        self.assertTrue(return_)
+
+    def test_valid_ebit(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+        wege.fill_ebit()
+        return_ = wege.valid_ebit()
+
+        self.assertIsNotNone(return_)
+        self.assertTrue(return_)
+
+        wege.ebit = -1
+        return_ = wege.valid_ebit()
+        self.assertIsNotNone(return_)
+        self.assertFalse(return_)
+
+    def test_valid_industry(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+        return_ = wege.valid_industry()
+
+        self.assertIsNotNone(return_)
+        self.assertTrue(return_)
+        wege.industry = 'Banks—Regional'
+
+        return_ = wege.valid_industry()
+
+        self.assertIsNotNone(return_)
+        self.assertFalse(return_)
+
+    def test_valid_information_dict(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+        return_ = wege.valid_information_dict()
+
+        self.assertIsNotNone(return_)
+        self.assertTrue(return_)
+
+        wege.all_modules = 'a'
+        return_ = wege.valid_information_dict()
+
+        self.assertFalse(return_)
+
+    def test_fill_ticker_info(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+        wege.get_ticker_data()
+
+        self.assertIsNotNone(wege.ticker_info)
+        self.assertIsInstance(wege.ticker_info, TICKER_INFO)
+
+    def test_get_ticker_data_false_valid_dict(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+
+        wege.all_modules = 'a'
+        return_ = wege.get_ticker_data()
+
+        self.assertIsNotNone(return_)
+        self.assertFalse(return_)
+
+    def test_get_ticker_data_false_valid_industry(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+
+        wege.industry = 'Banks—Regional'
+        return_ = wege.get_ticker_data()
+
+        self.assertIsNotNone(return_)
+        self.assertFalse(return_)
+
+    def test_get_ticker_data_false_valid_ebit(self):
+        wege = MagicFormula('WEGE3.SA', self.logger)
+        wege.get_ticker_info()
+
+        wege.ebit = -1
+        return_ = wege.get_ticker_data()
+
+        self.assertIsNotNone(return_)
+        self.assertFalse(return_)
