fea9da110ceab80372104d81e88c10983f689c34
marinellirubens <marinelli.rubens@gmail.com>
2021-07-14 08:15:29 -0300

    Tests removed due to change on main process

--- tests/test_stocks_greenblat_magic_formula.py
+++ tests/test_stocks_greenblat_magic_formula.py
@@ -21,32 +21,3 @@ from src.stocks_greenblat_magic_formula import logging
 def scenario_logger():
     logger = logging.getLogger(__name__)
     return logger
-
-
-class TestStocksGreenblatMagicFormula(unittest.TestCase):
-    def test_get_ticker_info_assert_is_dict(self):
-        logger = scenario_logger()
-        logger: logging.Logger = green.set_logger(logger)
-        
-        stock = green.get_ticker_info('ITSA4.SA', logger)
-        self.assertIsInstance(stock, yahooquery.Ticker)
-
-    def test_get_ticker_info_assert_is_none(self):
-        logger = scenario_logger()
-        logger: logging.Logger = green.set_logger(logger)
-        
-        stock = green.get_ticker_info('ITSA4.SA', logger)
-        self.assertIsNotNone(stock)
-
-    def test_get_ticker_data(self):
-        pass
-
-    def test_get_recomendation_trend(self):
-        logger = scenario_logger()
-        logger: logging.Logger = green.set_logger(logger)
-
-        ticker = green.get_ticker_info('ITSA4.SA', logger)
-        recomendation_trend = green.get_recomendation_trend(ticker=ticker)
-
-        self.assertIsNotNone(recomendation_trend)
-        self.assertIsInstance(recomendation_trend, tuple)
