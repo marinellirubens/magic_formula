bad072dcc788148d15730c154e3a54007c09f7b4
marinellirubens <marinelli.rubens@gmail.com>
2021-07-17 09:26:16 -0300

    Included tests for the function get_arguments

--- tests/test_config.py
+++ tests/test_config.py
@@ -14,6 +14,7 @@ except:
 from src.config import logging
 from src.config import get_config
 from src.config import set_logger
+from src.config import get_arguments
 
 
 def scenario_logger():
@@ -39,3 +40,26 @@ class TestSetup(unittest.TestCase):
         format = '%(asctime)s - %(name)s - (%(threadName)-10s) - %(levelname)s - %(message)s'
         for handler in logger_formated.handlers:
             self.assertEqual(handler.formatter._fmt, format)
+
+    def test_get_arguments_defaults(self):
+        options = get_arguments([])
+        self.assertFalse(options.version)
+        self.assertEqual(options.index, 'ibrx_100')
+        self.assertEqual(options.ebit, 0)
+        self.assertEqual(options.market_cap, 0)
+        self.assertEqual(options.qty, 15)
+
+    def test_get_arguments_arguments(self):
+        arguments = [
+            [['-V'],[True, 'ibrx_100', 0, 0, 15]],
+            [['-e', '2'],[False, 'ibrx_100', 2, 0, 15]],
+            [['-i', 'teste', '-m', '50000'],[False, 'teste', 0, 50000, 15]],
+            [['-i', 'teste', '-m', '50001', '-q', '100'],[False, 'teste', 0, 50001, 100]],
+        ]
+        for args in arguments:
+            options = get_arguments(args[0])
+            self.assertEqual(options.version, args[1][0])
+            self.assertEqual(options.index, args[1][1])
+            self.assertEqual(options.ebit, args[1][2])
+            self.assertEqual(options.market_cap, args[1][3])
+            self.assertEqual(options.qty, args[1][4])
