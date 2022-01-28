158e05bffa915131fc77d7e74d51b9b1b28aae1c
marinellirubens <marinelli.rubens@gmail.com>
2022-01-27 22:39:05 -0300

    Updated Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='magic_formula',
-    version='1.0.1',
+    version='1.0.3',
     packages=find_packages(),
     entry_points={
         "console_scripts": [
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/marinellirubens/magic_formula',
     author='Rubens Marinelli Ferreira',
     author_email='marinelli.rubens@gmail.com',
-    description='Program to implement joel greenbla magic formula',
+    description="Program to implement joel greenblat's magic formula",
     install_requires=[
         'pandas>=1.1.4',
         'openpyxl>=2.0.7',
