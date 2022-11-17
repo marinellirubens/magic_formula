9c6802226637b52fdb308bb35c544d880c2c3999
marinellirubens <marinelli.rubens@gmail.com>
2022-11-17 20:08:54 -0300

    Included headers on get_ticker_roic_info

--- magic_formula/status_invest.py
+++ magic_formula/status_invest.py
@@ -39,7 +39,22 @@ def get_ticker_roic_info(url: str) -> dict:
     :return: Dictionary with index informations
     :rtype: dict
     """
-    tickers_info = requests.get(url).content
+    headers = {
+        'authority': 'statusinvest.com.br',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    }
+    tickers_info = requests.get(url, headers=headers).content
 
     roic_info_df: pandas.DataFrame = pandas.read_json(io.StringIO(tickers_info.decode('utf-8')))
     roic_info_df = roic_info_df.sort_values('roic', ascending=False)
